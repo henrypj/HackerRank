4f7c5b52ac6fc066a2f4fb6a04e9fbc6ac09b744
Paul Henry <Paul Henry>
2017-08-31 19:29:31 +0100

    Starting to add to Strings

--- Implementation/QueensAttack.py
+++ Implementation/QueensAttack.py
@@ -2,23 +2,124 @@
 
 import sys
 """
+# Description
+# Difficulty: Medium
+#
+# A queen is standing on an n x n chessboard. The chessboard's rows are numbered
+# from 1 to n, going from bottom to top; its columns are numbered from 1 to n, 
+# going from left to right. Each square on the board is denoted by a tuple, (r, c), 
+# describing the row, r, and column, c, where the square is located.
+#
+# The queen is standing at position (r(q), c(q)) and, in a single move, she can
+# attack any square in any of the eight directions (left, right, up, down, or 
+# the four diagonals). In the diagram below, the "O" denote all the cells the
+# queen can attack from :
+#
+#          +---+---+---+---+---+---+---+---+
+#        8 |   |   |   | O |   |   |   | O |
+#          +---+---+---+---+---+---+---+---+
+#        7 | X |   |   | O |   |   | O |   |
+#          +---+---+---+---+---+---+---+---+
+#        6 |   | X |   | O |   | X |   |   |
+#          +---+---+---+---+---+---+---+---+
+#        5 |   |   | X | O | X |   |   |   |
+#          +---+---+---+---+---+---+---+---+
+#        4 | X | X | X | # | X | X | X | X |
+#          +---+---+---+---+---+---+---+---+
+#        3 |   |   | X | O | X |   |   |   |
+#          +---+---+---+---+---+---+---+---+
+#        2 |   | X |   | O |   | X |   |   |
+#          +---+---+---+---+---+---+---+---+
+#        1 | X |   |   | O |   |   | X |   |
+#          +---+---+---+---+---+---+---+---+
+#            1   2   3   4   5   6   7   8
+#
+# There are K obstacles on the chessboard preventing the queen from attacking 
+# any square that has an obstacle blocking the the queen's path to it. For 
+# example, an obstacle at location (3,5) in the diagram above would prevent the
+# queen from attacking cells (3,5), (2,6), and (1,7):
+#
+#          +---+---+---+---+---+---+---+---+
+#        8 |   |   |   | X |   |   |   | X |
+#          +---+---+---+---+---+---+---+---+
+#        7 | X |   |   | X |   |   | X |   |
+#          +---+---+---+---+---+---+---+---+
+#        6 |   | X |   | X |   | X |   |   |
+#          +---+---+---+---+---+---+---+---+
+#        5 |   |   | X | X | X |   |   |   |
+#          +---+---+---+---+---+---+---+---+
+#        4 | X | X | X | Q | X | X | X | X |
+#          +---+---+---+---+---+---+---+---+
+#        3 |   |   | X | X | * |   |   |   |
+#          +---+---+---+---+---+---+---+---+
+#        2 |   | X |   | X |   | * |   |   |
+#          +---+---+---+---+---+---+---+---+
+#        1 | X |   |   | X |   |   | * |   |
+#          +---+---+---+---+---+---+---+---+
+#            1   2   3   4   5   6   7   8
+#
+# Given the queen's position and the locations of all the obstacles, find and 
+# print the number of squares the queen can attack from her position at (r(q),c(q)).
 #
 # Input Format
 #
+# The first line contains two space-separated integers describing the respective
+# values of n (the side length of the board) and k (the number of obstacles). 
+#
+# The next line contains two space-separated integers describing the respective
+# values of r(q) and c(q), denoting the position of the queen. 
+#
+# Each line i of the k subsequent lines contains two space-separated integers 
+# describing the respective values of and r(i), c(i) denoting the position of 
+# obstacle i.
 #
 # Constraints
 #
+# 0 <= n <= 10**5
+# 0 <= k <= 10**5
+# A single cell may contain more than one obstacle; however, it is guaranteed 
+# that there will never be an obstacle at position (r(q), c(q)) where the queen
+# is located.
+#
+# Subtasks
+#
+# For 30% of the maximum score
+# 0 <= n <= 100
+# 0 <= k <= 100
+#
+# For 50% of the maximum score
+# 0 <= n <= 1000
+# 0 <= k <= 10**5
 #
 # Output Format 
 #
+# Print the number of squares that the queen can attack from position (r(q),c(q)).
 #
 # Example
 #
 # Given Input:
+# 4 0
+# 4 4
 #
 # Output:
+# 9
 #
 # Explanation:
+#
+# The queen is standing at position (4,3) on a 5 x 5 chessboard with no obstacles:
+#
+#          +---+---+---+---+---+
+#        5 |   |   |   |   |   |
+#          +---+---+---+---+---+
+#        4 |   |   | Q |   |   |
+#          +---+---+---+---+---+
+#        3 |   |   |   |   |   |
+#          +---+---+---+---+---+
+#        2 |   |   |   |   |   |
+#          +---+---+---+---+---+
+#        1 |   |   |   |   |   |
+#          +---+---+---+---+---+
+#            1   2   3   4   5  
 #
 # Solution
 # Save for later...
