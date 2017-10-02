8a3f5ed8f0730cf632bb4a94b12d0067ccc53b9f
Paul Henry <Paul Henry>
2017-10-03 00:42:24 +0100

    Removing ComputerGame.py and Factors.py

--- Factors.py
+++
@@ -1,7 +0,0 @@
-# Returns all factors of a given number n
-import functools
-
-def factors(n):    
-    return set(functools.reduce(list.__add__,([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
-
-print(factors(96))
